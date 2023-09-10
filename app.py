6c31752019c40c84f1cea1408b8e77038524ed6d
sp <sachinpatel7496007@gmail.com>
2023-09-10 18:19:37 +0530

    dvc init

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
-# from src.mlproject.logger import logging
-# from src.mlproject.exception import CustomException
-# import sys
-# from src.mlproject.components.data_intestion import DataIngestion
-# #from src.mlproject.components.data_intestion import DataIngestionConfig
-
-
-
-# if __name__=="__main__":
-#     logging.info("The ececution is started")
-    
-#     try:
-#         #data_intestion_config=DataIngestionConfig()        
-#         data_intestion=DataIngestion()  
-#         data_intestion.initiate_data_ingestion()
-        
-#     except Exception as e:
-#         logging.info("Custom exception")
-#         raise CustomException(e,sys)
-
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_intestion import DataIngestion
+
 
 import sys
 
