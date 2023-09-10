90a68b5ff18dd142129884088aaec52d70115e3a
sp <sachinpatel7496007@gmail.com>
2023-09-10 17:30:59 +0530

    add data

---
+++ setup.py
@@ -0,0 +1,11 @@
+# from setuptools import find_packages,setup
+# from typing import Union
+
+# setup(
+#     name="mlproject",
+#     version="0.0.1",
+#     author="Sachin",
+#     author_email="sachin@gmail.com",
+#     packages=find_packages(),
+#     install_requirement = ['pandas','numpy']
+# )
